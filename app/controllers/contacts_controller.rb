f3b6682e1c1ad7ecee5eb19938732592c2f065f8
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-07-27 21:13:09 +0100

    Add some pdf test

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -15,8 +15,25 @@ class ContactsController < ApplicationController
       format.html
       format.pdf do
         pdf = Prawn::Document.new
+        y_position = pdf.cursor
         pdf.text 'Hello World!'
         # pdf = PDFS::PdfTests.new
+        # pdf.table([["foo", "bar", "baz"], [{content: "foo",colspan: 2}]]) do |t|
+        #   t.cells.border_width = 1
+        #   t.before_rendering_page do |page|
+        #     page.row(0).border_top_width = 3
+        #     page.row(-1).border_bottom_width = 3
+        #     # page.column(0).border_left_width
+        #     # page.column(-1).border_right_width
+        #   end
+        # end
+        pdf.bounding_box([300, y_position], width: 200) do
+          pdf.text 'This bounding box has variable height. No matter how much text is ' \
+          'written here, the height will expand to fit.'
+          pdf.text ' _' * 1000
+          pdf.text ' *' * 1000
+          pdf.transparent(0.5) { pdf.stroke_bounds }
+        end
         send_data pdf.render, filename: 'pdf-test.pdf', type: 'application/pdf', disposition: 'inline'
       end
     end
